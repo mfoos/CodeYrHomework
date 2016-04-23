01d1a82b211ca8af194e664d9b8afc97f14242c9
Marianna Foos <marianna.foos@gmail.com>
2016-04-23 12:59:23 -0400

    initial commit

---
+++ heapify_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from heapify import heapify
+
+class heapify_test(unittest.TestCase):
+
+    def test_empty_array(self):
+        self.assertListEqual([], heapify([]))
+
+    def test_array_of_one(self):
+        self.assertListEqual([5], heapify([5]))
+
+    def test_is_integer_array(self):
+        pass
+
+    def test_example1(self):
+        self.assertListEqual([84, 22, 19, 10, 3, 17, 6, 5, 9], 
+        heapify([5, 3, 17, 10, 84, 19, 6, 22, 9]))
+
+if __name__ == '__main__':
+    unittest.main()
