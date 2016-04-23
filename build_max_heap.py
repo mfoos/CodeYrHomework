2db6e6267a0b4a11a3a26d5182a8bb616d034ebd
Marianna Foos <marianna.foos@gmail.com>
2016-04-22 20:50:54 -0400

    Removed redundant clauses, fixed for python2

--- build_max_heap.py
+++ build_max_heap.py
@@ -2,7 +2,7 @@ import math as m
 
 origArr = [5, 3, 17, 10, 84, 19, 6, 22, 9]
 
-start = m.floor(len(origArr)/2)-1
+start = int(m.floor(len(origArr)/2)-1)
 
 def left(A,i):
     return 2*(i+1) - 1
@@ -26,15 +26,13 @@ def sortHeapWithRoot(array, root):
         if right(array, root) > len(array) - 1:
             # if no RC
             idx = left(array, root)
-            maxx = array[idx]
         if array[left(array, root)] > array[right(array, root)]:
             # if LC > RC
             idx = left(array, root)
-            maxx = array[idx]
         if array[right(array, root)] > array[left(array,root)]:
             # if RC > LC and by extension P
             idx = right(array, root)
-            maxx = array[idx]
+        maxx = array[idx]
     elif right(array, root) > len(array) - 1:
         return
     elif array[right(array, root)] > array[root]:
@@ -51,6 +49,3 @@ def sortHeapWithRoot(array, root):
 while start >= 0:
     sortHeapWithRoot(origArr, start)
     start -= 1
-
-print(origArr)
-# gotta figure out how this scoping works
